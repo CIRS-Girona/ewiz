11926ba17f4122f24306a54d74f1004c4bd65a92
jad-mansour <jad_mansour1998@outlook.com>
2024-06-26 11:35:20 +0200

    Add converters skeleton

---
+++ src/ewiz/data/converters/base.py
@@ -0,0 +1,32 @@
+import os
+import h5py
+import hdf5plugin
+import numpy as np
+
+from tqdm import tqdm
+
+from typing import Any, Dict, List, Tuple, Callable, Union
+
+
+class ConvertBase():
+    """Base data converter.
+    """
+    def __init__(
+        self,
+        data_dir: str,
+        out_dir: str
+    ) -> None:
+        self.data_dir = data_dir
+        self.out_dir = out_dir
+        self._init_events()
+        self._init_images()
+
+    def _init_events(self) -> None:
+        """Initializes events file path.
+        """
+        raise NotImplementedError
+
+    def _init_images(self) -> None:
+        """Initializes images file path.
+        """
+        raise NotImplementedError
---
+++ src/ewiz/data/converters/mvsec.py
@@ -0,0 +1,15 @@
+import os
+import h5py
+import hdf5plugin
+import numpy as np
+
+from tqdm import tqdm
+
+from typing import Any, Dict, List, Tuple, Callable, Union
+
+
+class ConvertMVSEC():
+    """MVSEC to eWiz data converter.
+    """
+    def __init__(self) -> None:
+        pass
