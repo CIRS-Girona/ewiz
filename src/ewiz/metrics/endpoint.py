26f528280b9c9d51988646984ca684ca01b8bf2d
jad-mansour <jad_mansour1998@outlook.com>
2024-07-16 11:39:33 +0200

    Add main calculate function

--- src/ewiz/metrics/endpoint.py
+++ src/ewiz/metrics/endpoint.py
@@ -75,4 +75,47 @@ class EndpointError(MetricsBase):
     ) -> None:
         """Calculates angular error.
         """
-        raise NotImplementedError
+        pred_u, pred_v = pred_flow[:, 0, ...], pred_flow[:, 1, ...]
+        gt_u, gt_v = gt_flow[:, 0, ...], gt_flow[:, 1, ...]
+        self.metrics["ae"] = np.mean(np.sum(
+            np.arccos((1.0 + pred_u*gt_u + pred_v*gt_v)/
+            (np.sqrt(1.0 + pred_u*pred_u + pred_v*pred_v)*np.sqrt(1.0 + gt_u*gt_u + gt_v*gt_v))),
+            axis=(1, 2)
+        )/num_pixels)
+        self.sum_metrics["ae"] += self.metrics["ae"]
+
+    @MetricsBase.add_history
+    def calculate(
+        self,
+        pred_flow: np.ndarray,
+        gt_flow: np.ndarray,
+        encoded_events: np.ndarray = None,
+        delta_time: Union[float, np.ndarray] = None,
+        *args,
+        **kwargs
+    ) -> Dict[str, float]:
+        """Calculates endpoint error.
+        """
+        if len(pred_flow.shape) == 3:
+            pred_flow = pred_flow[None, ...]
+        if len(gt_flow.shape) == 3:
+            gt_flow = gt_flow[None, ...]
+
+        flow_mask = self.get_flow_mask(gt_flow)
+        if encoded_events is not None:
+            events_mask = self.get_events_mask(encoded_events)
+            total_mask = np.logical_and(flow_mask, events_mask)
+        else:
+            total_mask = flow_mask
+
+        pred_flow = pred_flow*total_mask
+        gt_flow = gt_flow*total_mask
+        num_pixels = np.sum(total_mask, axis=(1, 2, 3)) + 1e-5
+
+        if delta_time is not None:
+            pred_flow = self.convert_flow_velocity_to_displacement(pred_flow, delta_time)
+            gt_flow = self.convert_flow_velocity_to_displacement(gt_flow, delta_time)
+        self.calculate_endpoint_error(pred_flow, gt_flow, num_pixels)
+        self.calculate_angular_error(pred_flow, gt_flow, num_pixels)
+        self.count += 1
+        return self.metrics
