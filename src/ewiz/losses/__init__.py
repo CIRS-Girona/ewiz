097a0d5eb842c9543ecf1c5824ad0912dffc9ec8
Jad Mansour <jad_mansour1998@outlook.com>
2024-09-12 12:14:53 +0200

    Add hybrid loss

--- src/ewiz/losses/__init__.py
+++ src/ewiz/losses/__init__.py
@@ -1,1 +1,21 @@
+from ewiz.core.utils import get_inheritors
+
+# Base loss import
 from .base import LossBase
+
+# Image variance loss imports
+from .variance.variance import ImageVariance
+from .variance.normalized import NormalizedImageVariance
+from .variance.multifocal import MultifocalNormalizedImageVariance
+
+# Gradient magnitude loss imports
+from .gradient.gradient import GradientMagnitude
+from .gradient.normalized import NormalizedGradientMagnitude
+from .gradient.multifocal import MultifocalNormalizedGradientMagnitude
+
+# TODO: Check photometric loss format
+from .photometric import Photometric
+
+
+# Import all losses
+loss_functions = get_inheritors(parent=LossBase)
