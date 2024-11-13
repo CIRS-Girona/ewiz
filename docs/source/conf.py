60f90f46126aa5b712be7f1ce3184683d8f6be48
Jad Mansour <jad_mansour1998@outlook.com>
2024-11-13 10:12:21 +0100

    Add logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,5 @@
+import os
+
 project = "eWiz"
 copyright = "2024, Jad Mansour, University of Girona. All rights reserved"
 author = "Jad Mansour"
@@ -7,3 +9,9 @@ extensions = ["sphinx_rtd_theme", "sphinx.ext.autodoc"]
 autodoc_member_order = "bysource"
 
 html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+html_logo = os.path.join("_static", "logo_no_slogan.svg")
+html_theme_options = {
+    "logo_only": True,
+    "display_version": True
+}
