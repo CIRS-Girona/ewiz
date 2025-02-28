b983e740e8f4189a954de7b55e9c41bcd5de1514
Jad Mansour <jad_mansour1998@outlook.com>
2025-02-28 08:40:17 +0100

    Fix flow synchronizer (unit conversion)

--- scripts/read_mvsec.py
+++ scripts/read_mvsec.py
@@ -6,8 +6,7 @@ from ewiz.data.readers import ReaderFlow
 
 
 if __name__ == "__main__":
-    data_dir = "/home/jad/Documents/datasets/converted/mvsec/indoor_flying1"
+    data_dir = "/home/jad/datasets/mvsec/ewiz/indoor_flying1"
     clip_mode = "images"
     data = ReaderFlow(data_dir=data_dir, clip_mode=clip_mode)
-    events, gray_images, gray_time, flow = data[100:140]
-    print(events, gray_images, gray_time, flow)
+    events, gray_images, gray_time, flow = data[100:120]
